9bef543cf1dc15b3207ca657622ee8849f87a79c
SomeSourceCode <somesourcecode@gmail.com>
2024-03-16 23:50:35 +0100

    feat: checkerboard background pattern

--- src/main/java/io/github/somesourcecode/someguiapi/scene/Background.java
+++ src/main/java/io/github/somesourcecode/someguiapi/scene/Background.java
@@ -6,8 +6,12 @@ public interface Background {
 
 	 ItemStack backgroundAt(int layoutX, int layoutY);
 
-	 static Background staticBackground(GuiItem item) {
+	 static Background fill(GuiItem item) {
 		return (layoutX, layoutY) -> item.asItemStack();
+	 }
+
+	 static Background checkerboard(GuiItem primary, GuiItem secondary) {
+		 return (layoutX, layoutY) -> (layoutX + layoutY) % 2 == 0 ? primary.asItemStack() : secondary.asItemStack();
 	 }
 
 }
