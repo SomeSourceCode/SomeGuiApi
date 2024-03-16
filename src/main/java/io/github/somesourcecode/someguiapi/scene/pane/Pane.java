6cd390fae21a25a2263d61c583ec36a21819fc7b
SomeSourceCode <somesourcecode@gmail.com>
2024-03-16 23:01:58 +0100

    fix: remove unused import

--- src/main/java/io/github/somesourcecode/someguiapi/scene/pane/Pane.java
+++ src/main/java/io/github/somesourcecode/someguiapi/scene/pane/Pane.java
@@ -2,7 +2,6 @@ package io.github.somesourcecode.someguiapi.scene.pane;
 
 import io.github.somesourcecode.someguiapi.collections.ObservableList;
 import io.github.somesourcecode.someguiapi.scene.Node;
-import io.github.somesourcecode.someguiapi.scene.Parent;
 
 public class Pane extends Region {
 
