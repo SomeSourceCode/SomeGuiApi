c112136df2563cc3ae24e1a32bb9d7a2dcf1d92c
SomeSourceCode <somesourcecode@gmail.com>
2024-03-23 16:06:57 +0100

    docs: add JavaDocs for Pane

--- src/main/java/io/github/somesourcecode/someguiapi/scene/pane/Pane.java
+++ src/main/java/io/github/somesourcecode/someguiapi/scene/pane/Pane.java
@@ -3,6 +3,18 @@ package io.github.somesourcecode.someguiapi.scene.pane;
 import io.github.somesourcecode.someguiapi.collections.ObservableList;
 import io.github.somesourcecode.someguiapi.scene.Node;
 
+/**
+ * Pane does not modify the layout of its children in any way.
+ * However, it is exposing the children list, so child nodes
+ * can be added and removed freely.
+ * <p>
+ * Layout panes should extend this class.
+ * <p>
+ * For more complex layouts, use different panes, e.g.
+ * {@link io.github.somesourcecode.someguiapi.scene.pane.VBox},
+ * {@link io.github.somesourcecode.someguiapi.scene.pane.HBox},
+ * {@link io.github.somesourcecode.someguiapi.scene.pane.FlowPane}, etc.
+ */
 public class Pane extends Region {
 
 	@Override
