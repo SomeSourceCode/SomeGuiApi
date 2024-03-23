5bf360d1ef65927bcf589ab14a0db5d536e29440
SomeSourceCode <somesourcecode@gmail.com>
2024-03-23 16:38:16 +0100

    docs: add JavaDocs for Gui

--- src/main/java/io/github/somesourcecode/someguiapi/scene/gui/Gui.java
+++ src/main/java/io/github/somesourcecode/someguiapi/scene/gui/Gui.java
@@ -5,14 +5,27 @@ import org.bukkit.inventory.Inventory;
 
 import java.util.List;
 
+/**
+ * The base class for GUIs that can be shown to players.
+ */
 public abstract class Gui {
 
 	protected Inventory inventory;
 
+	/**
+	 * Shows this GUI to the specified player.
+	 * @param player the player
+	 */
 	public abstract void show(Player player);
 
+	/**
+	 * Returns a list of players that are currently viewing this GUI.
+	 */
 	public abstract List<Player> getViewers();
 
+	/**
+	 * Updates the GUI for all viewers.
+	 */
 	public void update() {
 		for (Player viewer : getViewers()) {
 			show(viewer);
