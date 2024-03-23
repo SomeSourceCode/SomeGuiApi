02f0365ae8249383d0bb1623ca6a49e5bbfd0456
SomeSourceCode <somesourcecode@gmail.com>
2024-03-23 16:19:35 +0100

    docs: add JavaDocs for VBox

--- src/main/java/io/github/somesourcecode/someguiapi/scene/pane/VBox.java
+++ src/main/java/io/github/somesourcecode/someguiapi/scene/pane/VBox.java
@@ -2,14 +2,25 @@ package io.github.somesourcecode.someguiapi.scene.pane;
 
 import io.github.somesourcecode.someguiapi.scene.Node;
 
+/**
+ * A layout pane that arranges its children in a vertical column.
+ */
 public class VBox extends Pane {
 
 	private int spacing;
 
+	/**
+	 * Returns the amount of vertical space between each child node.
+	 * @return the vertical spacing between children
+	 */
 	public int getSpacing() {
 		return spacing;
 	}
 
+	/**
+	 * Sets the amount of vertical space between each child node.
+	 * @param spacing the vertical spacing between children
+	 */
 	public void setSpacing(int spacing) {
 		this.spacing = spacing;
 	}
