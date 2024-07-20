e9db7578b70666eec625013cb2f868ab96362e6c
SomeSourceCode <somesourcecode@gmail.com>
2024-07-20 20:19:54 +0200

    feat: add BarrelGui

---
+++ src/main/java/io/github/somesourcecode/someguiapi/scene/gui/type/BarrelGui.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2024, SomeSourceCode - MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the “Software”), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package io.github.somesourcecode.someguiapi.scene.gui.type;
+
+import io.github.somesourcecode.someguiapi.scene.DirtyFlag;
+import io.github.somesourcecode.someguiapi.scene.gui.Gui;
+import io.github.somesourcecode.someguiapi.scene.gui.SceneGui;
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.event.inventory.InventoryType;
+import org.bukkit.inventory.Inventory;
+
+/**
+ * A GUI represented by a barrel inventory.
+ *
+ * @since 3.0.0
+ */
+public class BarrelGui extends SceneGui {
+
+	/**
+	 * Constructs a new BarrelGui with the specified title.
+	 *
+	 * @param title the title
+	 * @since 3.0.0
+	 */
+	public BarrelGui(Component title) {
+		this.title = title;
+
+		this.inventory = createInventory();
+		setDirtyFlag(DirtyFlag.GUI_CONTENT);
+	}
+
+	/**
+	 * Constructs a new BarrelGui with the specified title.
+	 *
+	 * @param title the title
+	 * @since 3.0.0
+	 */
+	public BarrelGui(String title) {
+		this(title == null ? null : Component.text(title));
+	}
+
+	@Override
+	public int getWidth() {
+		return 9;
+	}
+
+	@Override
+	public int getHeight() {
+		return 3;
+	}
+
+	@Override
+	public Inventory createInventory() {
+		Inventory inventory = title == null ? Bukkit.createInventory(null, InventoryType.BARREL)
+				: Bukkit.createInventory(null, InventoryType.BARREL, title);
+		Gui.addInventoryMapping(inventory, this);
+		return inventory;
+	}
+
+}
